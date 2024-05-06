0caf809011ed59f1d3e4f182705b48ffee1b0840
bryanserfozo <bryan.serfozo@revature.com>
2024-05-05 22:54:59 -0400

    Adding src filed

---
+++ src/common-functions.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const commonFunctions = {
+    validateSession: async () => {
+        try {
+            let res = await axios.get('http://ec2-54-172-227-238.compute-1.amazonaws.com/users/session', {
+                withCredentials: true
+            });
+            console.log(res);
+            if (res.status === 200) {
+                console.log("Session is valid");
+                return true;
+            } else{
+                console.log("Session is invalid");
+                return false;
+            }
+        } catch (error) {
+            console.log("Session is invalid");
+            return false;
+        }
+    },
+}
